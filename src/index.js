7ee6e8b5d97fe8f8133d11ebc782143567837fa8
Kunal <kdshah6593@gmail.com>
2021-02-16 16:22:03 -0500

    commit on 'master'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const taskList = document.getElementById("tasks");
+    const taskDescription = document.getElementById("new-task-description");
+    const newTaskForm = document.getElementById("create-task-form");
+    
+    
+    newTaskForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      const newli = document.createElement("LI");
+      const textnode = document.createTextNode(taskDescription.value);
+      newli.appendChild(textnode);
+      
+      newli.innerHTML += `<button id=${taskDescription.value}>X</button>`
+
+      taskList.appendChild(newli);
+    });
+
+    taskList.addEventListener("click", function(event) {
+      if (event.target.nodeName === "BUTTON") {
+        event.target.parentElement.remove();
+      }
+    })
+
 });
